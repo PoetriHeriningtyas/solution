a141dbeaee73c2534888b1afaa039c52a2858ff0
PoetriHeriningtyas <52372328+PoetriHeriningtyas@users.noreply.github.com>
2022-09-21 10:50:03 +0700

    denormalize historical table

--- src/Visualization.py
+++ src/Visualization.py
@@ -5,12 +5,17 @@ import glob
 import datetime
 
 MAIN_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)))
+pd.set_option('display.max_columns', 30)
 
 class Vizualization:
     def __init__(self):
         self.file_list_accounts = glob.glob(os.path.join(MAIN_DIR, 'data\\accounts\\','*.json'))
         self.file_list_cards = glob.glob(os.path.join(MAIN_DIR, 'data\\cards\\','*.json'))
         self.file_list_saving_accounts = glob.glob(os.path.join(MAIN_DIR, 'data\\savings_accounts\\','*.json'))
+        self.df_accounts = None
+        self.df_cards = None
+        self.df_saving_accounts = None
+
 
     def create_historical_table_accounts(self):
         info_account_hist = list()
@@ -27,8 +32,8 @@ class Vizualization:
                         "account_id": data_accounts.get("set", {}).get("account_id", str()),
                         "name": data_accounts.get("set", {}).get("name", str()),
                         "address": data_accounts.get("set", {}).get("address", str()),
-                        "phone_number": data_accounts.get("set", {}).get("account_id", str()),
-                        "email": data_accounts.get("set", {}).get("phone_number", str()),
+                        "phone_number": data_accounts.get("set", {}).get("phone_number", str()),
+                        "email": data_accounts.get("set", {}).get("email", str()),
                         "savings_account_id": data_accounts.get("set", {}).get("savings_account_id", str()),
                         "credit_used": data_accounts.get("set", {}).get("credit_used", str()),
                         "card_id":data_accounts.get("set", {}).get("card_id", str())
@@ -44,17 +49,16 @@ class Vizualization:
                         "account_id": data_accounts.get("data", {}).get("account_id", str()),
                         "name": data_accounts.get("data", {}).get("name", str()),
                         "address": data_accounts.get("data", {}).get("address", str()),
-                        "phone_number": data_accounts.get("data", {}).get("account_id", str()),
-                        "email": data_accounts.get("data", {}).get("phone_number", str()),
+                        "phone_number": data_accounts.get("data", {}).get("phone_number", str()),
+                        "email": data_accounts.get("data", {}).get("email", str()),
                         "savings_account_id": data_accounts.get("data", {}).get("savings_account_id", str()),
                         "credit_used": data_accounts.get("data", {}).get("credit_used", str()),
                         "card_id":data_accounts.get("data", {}).get("card_id", str())
                         }
                     }
             info_account_hist.append(info_accounts)
-            df_accounts = pd.json_normalize(info_account_hist)
-        print(df_accounts)
-        #data.sort_values(by='AdmissionDate')
+            self.df_accounts = pd.json_normalize(info_account_hist)
+        print(self.df_accounts)
 
     def create_historical_table_cards(self):
         info_card_hist = list()
@@ -68,7 +72,7 @@ class Vizualization:
                     "op": data_cards.get("op", str()),
                     "ts": self.timestamp_to_stringdate(data_cards.get("ts")),
                     "data": {
-                        "card_id": data_cards.get("set", {}).get("card_id", str()),
+                        "card_id": data_cards.get("id", str())[:2],
                         "card_number": data_cards.get("set", {}).get("card_number", str()),
                         "credit_used": data_cards.get("set", {}).get("credit_used", 0),
                         "monthly_limit": data_cards.get("set", {}).get("monthly_limit", int()),
@@ -90,8 +94,8 @@ class Vizualization:
                         }
                     }
             info_card_hist.append(info_cards)
-            df_cards = pd.json_normalize(info_card_hist)
-        print(df_cards)
+            self.df_cards = pd.json_normalize(info_card_hist)
+        print(self.df_cards)
     
     def create_historical_table_saving_accounts(self):
         info_saving_accounts_hist = list()
@@ -105,7 +109,7 @@ class Vizualization:
                     "op": data_saving_accounts.get("op", str()),
                     "ts": self.timestamp_to_stringdate(data_saving_accounts.get("ts")),
                     "data": {
-                        "savings_account_id": data_saving_accounts.get("set", {}).get("savings_account_id", str()),
+                        "savings_account_id": data_saving_accounts.get("id", str())[:3],
                         "balance": data_saving_accounts.get("set", {}).get("balance", int()),
                         "interest_rate_percent": data_saving_accounts.get("set", {}).get("interest_rate_percent", float()),
                         "status": data_saving_accounts.get("set", {}).get("status", str()),
@@ -125,8 +129,8 @@ class Vizualization:
                         }
                     }
             info_saving_accounts_hist.append(info_saving_accounts)
-            df_saving_accounts = pd.json_normalize(info_saving_accounts_hist)
-        print(df_saving_accounts)
+            self.df_saving_accounts = pd.json_normalize(info_saving_accounts_hist)
+        print(self.df_saving_accounts)
     
     @staticmethod
     def timestamp_to_stringdate(ts):
@@ -134,11 +138,17 @@ class Vizualization:
         string_date = f"{value:%Y-%m-%d %H:%M:%S}"
         return string_date
     
-    def denormalized_table(self):
-        pass
+    def denormalized_historical_table(self):
+        denormalized1 = pd.merge(self.df_accounts, self.df_cards, how="outer", on=["data.card_id"])
+        denormalized1.loc[denormalized1['op_y'] == 'u', 'ts_x'] = denormalized1.loc[denormalized1['op_y'] == 'u', 'ts_y']
+        denormalized2 = pd.merge(denormalized1, self.df_saving_accounts, how="outer", on=["data.savings_account_id"])
+        denormalized2.loc[denormalized2['op'] == 'u', 'ts_x'] = denormalized2.loc[denormalized2['op'] == 'u', 'ts']
+        denormalized2.to_excel("denormalized2_outer.xlsx")
+
 
 if __name__ == '__main__':  
     viz = Vizualization()
     viz.create_historical_table_accounts()
     viz.create_historical_table_cards()
     viz.create_historical_table_saving_accounts()
+    viz.denormalized_historical_table()
